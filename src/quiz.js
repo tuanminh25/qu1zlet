ea659108da5481f8c756431078882654d8092184
Vincent Le <z5425868@ad.unsw.edu.au>
2023-09-20 14:06:08 +1000

    add stub for adminquiznameupdate

--- src/quiz.js
+++ src/quiz.js
@@ -1,23 +1,39 @@
 
-function adminQuizCreate(authUserId, name, description) {
-    return {
-        quizId: 2,
-    }
-  }
+/**
+    adminQuizDescriptionUpdate 
+    Update the description of the relevant quiz. 
+    Parameters:
+    ( authUserId, quizId, description ) 
+    Return object:
+    { } empty object  
+ * */
 
-  function adminQuizRemove(authUserId, quizId) {
-    return {
-    }
+
+function adminQuizDescriptionUpdate (authUserId, quizId, description) {
+  return {
+
   }
+}
+
+function adminQuizCreate(authUserId, name, description) {
+  return {
+      quizId: 2,
+  }
+}
+
+function adminQuizRemove(authUserId, quizId) {
+  return {
+  }
+}
 
 function adminQuizList(authUserId) {
-    return { quizzes: [
-        {
-          quizId: 1,
-          name: 'My Quiz',
-        }
-      ]
-    }
+  return { quizzes: [
+      {
+        quizId: 1,
+        name: 'My Quiz',
+      }
+    ]
+  }
 }
 
 function adminQuizInfo(authUserId, quizId) {
@@ -27,5 +43,10 @@ function adminQuizInfo(authUserId, quizId) {
     timeCreated: 1683125870,
     timeLastEdited: 1683125871,
     description: 'This is my quiz',
+  }
+}
+
+function adminQuizNameUpdate(authUserId, quizId, name){
+  return{
   }
 }
