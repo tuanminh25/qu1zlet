797921a1b3db8b5e9c65fa3323b87dad91eddd47
Brian <z5419667@ad.unsw.edu.au>
2023-09-20 14:04:56 +1000

    finished adminAuthRegister

---
+++ src/other.js
@@ -0,0 +1,14 @@
+/*
+    Reset the state of the application back to the start. 
+    Parameters:
+    () no parameters 
+    Return object:
+    { } empty object
+
+*/
+
+function clear() {
+    return {
+        
+    }
+}
---
+++ src/quiz.js
@@ -0,0 +1,38 @@
+
+/**
+    adminQuizDescriptionUpdate 
+    Update the description of the relevant quiz. 
+    Parameters:
+    ( authUserId, quizId, description ) 
+    Return object:
+    { } empty object  
+ * */
+
+
+function adminQuizDescriptionUpdate (authUserId, quizId, description) {
+  return {
+
+  }
+}
+
+function adminQuizCreate(authUserId, name, description) {
+  return {
+      quizId: 2,
+  }
+}
+
+function adminQuizRemove(authUserId, quizId) {
+  return {
+  }
+}
+
+function adminQuizList(authUserId) {
+  return { quizzes: [
+      {
+        quizId: 1,
+        name: 'My Quiz',
+      }
+    ]
+  }
+}
+
