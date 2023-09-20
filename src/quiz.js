fe85ca92fca15a28e4faf7e586511a4530554481
Tracie Duong <z5420451@ad.unsw.edu.au>
2023-09-20 13:25:04 +1000

    creates stub code for adminQuizCreate and adminQuizRemove

---
+++ src/quiz.js
@@ -0,0 +1,10 @@
+function adminQuizCreate(authUserId, name, description) {
+    return {
+        quizId: 2,
+    }
+  }
+
+  function adminQuizRemove(authUserId, quizId) {
+    return {
+    }
+  }
