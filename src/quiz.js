f5e48f7d155e46cc192c1ef5a0506f727a730b8f
Vincent Le <z5425868@ad.unsw.edu.au>
2023-09-20 14:00:25 +1000

    add stub for adminquizinfo

--- src/quiz.js
+++ src/quiz.js
@@ -20,3 +20,12 @@ function adminQuizList(authUserId) {
     }
 }
 
+function adminQuizInfo(authUserId, quizId) {
+  return{
+    quizId: 1,
+    name: 'My Quiz',
+    timeCreated: 1683125870,
+    timeLastEdited: 1683125871,
+    description: 'This is my quiz',
+  }
+}
