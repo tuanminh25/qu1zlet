5efcdfe79761d89e5d2eed7619c24fe0c43e84d3
Roger Duong <z5428838@ad.unsw.edu.au>
2023-09-20 13:21:15 +1000

    finished adminUserDetails

---
+++ src/auth.js
@@ -0,0 +1,11 @@
+function adminUserDetails(authUserId) {
+    return { user:
+        {
+        userId: 1,
+        name: 'Hayden Smith',
+        email: 'hayden.smith@unsw.edu.au',
+        numSuccessfulLogins: 3,
+        numFailedPasswordsSinceLastLogin: 1,
+        }
+    }
+}
