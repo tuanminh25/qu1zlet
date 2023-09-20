0e44731d20caa6364e7c29a50d591e273b5958a7
Minh Truong <z5381092@ad.unsw.edu.au>
2023-09-20 13:49:03 +1000

    implement adminQuizDesUpdate

---
+++ src/quiz.js
@@ -0,0 +1,15 @@
+/**
+    adminQuizDescriptionUpdate 
+    Update the description of the relevant quiz. 
+    Parameters:
+    ( authUserId, quizId, description ) 
+    Return object:
+    { } empty object  
+ * */
+
+
+function adminQuizDescriptionUpdate (authUserId, quizId, description) {
+    return {
+
+    }
+}
