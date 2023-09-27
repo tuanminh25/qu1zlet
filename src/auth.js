a04235350cdb4d601a1b557cfaf04d0fab779c89
Roger <z5428838@ad.unsw.edu.au>
2023-09-27 17:04:16 +1000

    added documentation for adminAuthRegister an aminAuthLogin

--- src/auth.js
+++ src/auth.js
@@ -1,9 +1,25 @@
+/**
+  * Register a user with an email, password, and names,
+  *  then returns their authUserId value.
+  * @param {string} email
+  * @param {string} password 
+  * @param {string} nameFirst
+  * @param {string} nameLast 
+  * @returns {number} authUserId
+*/
 function adminAuthRegister(email, password, nameFirst, nameLast) {
   return {
     authUserId: 1
   }
 }
 
+/**
+  * Given a registered user's email and password
+  * returns their authUserId value.
+  * @param {string} email
+  * @param {string} password 
+  * @returns {number} authUserId
+*/
 function adminAuthLogin(email, password) {
   return {
     authUserId: 1
