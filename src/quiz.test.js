db2574c402947866cbf3244283fca8bc6d67f1dc
Tracie Duong <z5420451@ad.unsw.edu.au>
2023-09-30 01:09:49 +1000

    adds a test for adminquizcreate and includes the clear function

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -3,31 +3,21 @@ import {
   adminQuizDelete,
 } from './quiz.js';
 
+import {
+  clear,
+} from './other.js';
+
+beforeEach(() => {
+  clear();
+});
 
 describe('adminQuizCreate', () => {
-  let data = {
-    users: [
-      {
-        userId: 1,
-        nameFirst: 'Hayden',
-        nameLast: 'Smith',
-        email: 'hayden.smith@unsw.edu.au',
-        password: 'hayden123',
-        numSuccessfulLogins: 3,
-        numFailedPasswordsSinceLastLogin: 1,
-      }
-    ],
-    quizzes: [
-      {
-        quizId: 1,
-        name: 'My Quiz',
-        timeCreated: 1683125870,
-        timeLastEdited: 1683125871,
-        description: 'This is my quiz',
-        ownedBy: 1,
-      }    
-    ]
-  }
+  test("check for the correct return type", () => {
+    expect(adminQuizCreate(1, "The Prestige", "Christopher Nolan")).toStrictEqual({
+      movieId: expect.any(Number),
+    });
+  });
+
 
   describe("test that the quiz is stored in data", () => {
     const uid = adminQuizCreate(1, 'Planet of the Apes', 'Vincent is an ape.');
@@ -51,7 +41,13 @@ describe('adminQuizCreate', () => {
       toStrictEqual({error: expect.any(String)});
   });
 
-  const cases = [['', 'The Titanic', 'ship'], [1, '', 'two'], [1, 'Spiderman', '']];
+  const cases = [['', 'The Titanic', 'ship'],
+                 [1, '', 'two'], 
+                 [1, 'movieeeeee', ''],
+                 ['', '', 'i love computers'],
+                 [1, '', ''],
+                 ['', 'Spiderman', '']
+                 ['', '', ''],];
   describe("blank inputs should create an error", () => {
     test.each(cases)((firstArg, secondArg, thirdArg) => {
       expect(adminQuizCreate(firstArg, secondArg, thirdArg)).
