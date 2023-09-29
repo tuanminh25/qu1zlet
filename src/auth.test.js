60a86777f651944311e63de2be0623e6c130b38a
Roger Duong <z5428838@ad.unsw.edu.au>
2023-09-29 15:02:51 +1000

    added more tests for adminUserDetails

--- src/auth.test.js
+++ src/auth.test.js
@@ -2,20 +2,124 @@ import {
   adminAuthRegister,
   adminAuthLogin,
   adminUserDetails,
-} from './auth.js';
+} from './auth';
+
+import clear from './other'
+
+const ERROR = { error: expect.any(String) };
+
+beforeEach(() => {
+  clear();
+});
 
 describe('adminUserDetails', () => {
+  let user;
+  beforeEach(() => {
+    user = adminAuthRegister('roger@gmail.com', 'roger123', 'Roger', 'Duong');
+  });
+
   test('AuthUserId is not a valid user', () => {
-    expect(adminUserDetails(expect.any(Number))).toStrictEqual({error: expect.any(String)});
-  })
-  test('Valid authUserId', () => {
-    expect(adminUserDetails(expect.any(Number))).toStrictEqual({user :
+    expect(adminUserDetails(user.authUserId + 1)).toStrictEqual(ERROR);
+  });
+
+  test('Valid authUserId no log in', () => {
+    expect(adminUserDetails(user.authUserId)).toStrictEqual({user :
       {
-        userId: expect.any(String),
-        name : expect.any(String),
-        email : expect.any(String),
-        numSuccessfulLogins: expect.any(Number),
-        numFailedPasswordsSinceLastLogin: expect.any(Number)
+        userId: user.authUserId,
+        name : 'Roger Duong',
+        email : 'roger@gmail.com',
+        numSuccessfulLogins: 1,
+        numFailedPasswordsSinceLastLogin: 0,
+      }
+    });
+  });
+
+  test('Valid authUserId 1 successful login', () => {
+    adminAuthLogin('roger@gmail.com', 'roger123');
+    expect(adminUserDetails(user.authUserId)).toStrictEqual({user :
+      {
+        userId: user.authUserId,
+        name : 'Roger Duong',
+        email : 'roger@gmail.com',
+        numSuccessfulLogins: 2,
+        numFailedPasswordsSinceLastLogin: 0,
+      }
+    });
+  });
+
+  test('Valid authUserId multiple successful login', () => {
+    adminAuthLogin('roger@gmail.com', 'roger123');
+    adminAuthLogin('roger@gmail.com', 'roger123');
+    adminAuthLogin('roger@gmail.com', 'roger123');
+    expect(adminUserDetails(user.authUserId)).toStrictEqual({user :
+      {
+        userId: user.authUserId,
+        name : 'Roger Duong',
+        email : 'roger@gmail.com',
+        numSuccessfulLogins: 4,
+        numFailedPasswordsSinceLastLogin: 0,
+      }
+    });
+  });
+
+  test('Valid authUserId 1 failed log in', () => {
+    adminAuthLogin('roger@gmail.com', 'roger1234');
+    expect(adminUserDetails(user.authUserId)).toStrictEqual({user :
+      {
+        userId: user.authUserId,
+        name : 'Roger Duong',
+        email : 'roger@gmail.com',
+        numSuccessfulLogins: 1,
+        numFailedPasswordsSinceLastLogin: 1,
+      }
+    });
+  });
+
+  test('Valid authUserId multiple failed log in', () => {
+    adminAuthLogin('roger@gmail.com', 'roger1234');
+    adminAuthLogin('roger@gmail.com', 'messi1234');
+    adminAuthLogin('roger@gmail.com', 'neymar10');
+    expect(adminUserDetails(user.authUserId)).toStrictEqual({user :
+      {
+        userId: user.authUserId,
+        name : 'Roger Duong',
+        email : 'roger@gmail.com',
+        numSuccessfulLogins: 1,
+        numFailedPasswordsSinceLastLogin: 3,
+      }
+    });
+  });
+
+  test('Valid authUserId failed log in reset', () => {
+    adminAuthLogin('roger@gmail.com', 'roger1234');
+    adminAuthLogin('roger@gmail.com', 'messi1234');
+    adminAuthLogin('roger@gmail.com', 'neymar10');
+    adminAuthLogin('roger@gmail.com', 'roger123');
+    expect(adminUserDetails(user.authUserId)).toStrictEqual({user :
+      {
+        userId: user.authUserId,
+        name : 'Roger Duong',
+        email : 'roger@gmail.com',
+        numSuccessfulLogins: 2,
+        numFailedPasswordsSinceLastLogin: 0,
+      }
+    });
+  });
+
+  test('Valid authUserId failed log in reset then failed log in again ', () => {
+    adminAuthLogin('roger@gmail.com', 'roger1234');
+    adminAuthLogin('roger@gmail.com', 'messi1234');
+    adminAuthLogin('roger@gmail.com', 'neymar10');
+    adminAuthLogin('roger@gmail.com', 'roger123');
+    adminAuthLogin('roger@gmail.com', 'neymar11');
+    adminAuthLogin('roger@gmail.com', 'neymar10');
+    expect(adminUserDetails(user.authUserId)).toStrictEqual({user :
+      {
+        userId: user.authUserId,
+        name : 'Roger Duong',
+        email : 'roger@gmail.com',
+        numSuccessfulLogins: 2,
+        numFailedPasswordsSinceLastLogin: 2,
       }
     });
   });
