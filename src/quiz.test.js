9eebfa1777da6d7d88b3d2e6d982f27fc750c0fb
Tracie Duong <z5420451@ad.unsw.edu.au>
2023-09-30 03:58:55 +1000

    debugs quiz.test.js involving wrong function names, wrong parameters, etc

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -1,27 +1,38 @@
 import {
-  adminQuizCreate,
-  adminQuizDelete,
+  adminQuizCreate, 
+  adminQuizRemove
 } from './quiz.js';
 
+import clear from './other.js';
+
 import {
-  clear,
-} from './other.js';
+  adminAuthRegister,
+  adminAuthLogin,
+  adminUserDetails,
+} from './auth.js';
+
+import { getData, setData } from "./dataStore.js";
+let store  = getData();
+
+const ERROR = { error: expect.any(String) };
 
 beforeEach(() => {
   clear();
+  adminAuthRegister('hayden.smith@unsw.edu.au', 'password', 'nameFirst', 'nameLast');
+  adminAuthLogin('hayden.smith@unsw.edu.au', 'password');
 });
 
 describe('adminQuizCreate', () => {
+
   test("check for the correct return type", () => {
     expect(adminQuizCreate(1, "The Prestige", "Christopher Nolan")).toStrictEqual({
       movieId: expect.any(Number),
     });
   });
 
-
-  describe("test that the quiz is stored in data", () => {
+  test("test that the quiz is stored in data", () => {
     const uid = adminQuizCreate(1, 'Planet of the Apes', 'Vincent is an ape.');
-    expect(data.quizzes).toContainObject({
+    expect(store.quizzes).toContain({
       quizId: uid,
       name: 'Planet of the Apes',
       timeCreated: expect.any(Number),
@@ -31,32 +42,28 @@ describe('adminQuizCreate', () => {
     });
   });
 
-  describe("userid not contained in data", () => {
-    expect(adminQuizCreate(uid(), '21', 'whats 9 + 10')).
-    toStrictEqual({error: expect.any(String)})
+  test("userid not contained in data", () => {
+    expect(adminQuizCreate(Math.random(), '21', 'whats 9 + 10')).toStrictEqual(ERROR)
   });
 
-  describe("non-numerical input for id", () => {
-      expect(adminQuizCreate(1, 'The Notebook', 'Cry')).
-      toStrictEqual({error: expect.any(String)});
+  test("non-numerical input for id", () => {
+      expect(adminQuizCreate(1, 'The Notebook', 'Cry')).toStrictEqual(ERROR);
   });
 
-  const cases = [['', 'The Titanic', 'ship'],
-                 [1, '', 'two'], 
-                 [1, 'movieeeeee', ''],
-                 ['', '', 'i love computers'],
-                 [1, '', ''],
-                 ['', 'Spiderman', '']
-                 ['', '', ''],];
-  describe("blank inputs should create an error", () => {
-    test.each(cases)((firstArg, secondArg, thirdArg) => {
-      expect(adminQuizCreate(firstArg, secondArg, thirdArg)).
-      toStrictEqual({error: expect.any(String)})
-    });
+  test.each([
+    {a: '', b: 'The Titanic', c: 'ship'},
+    {a: 1, b: '', c: 'ship'}, 
+    {a: 1, b: 'The Titanic', c: ''},
+    {a: '', b: '', c: 'ship'},
+    {a: 1, b: '', c: ''},
+    {a: '', b: 'The Titanic', c: ''},
+    {a: '', b: '', c: ''},
+  ])('blank inputs should create an error', ({a, b, c}) => {
+    expect(adminQuizCreate(a, b, c)).toStrictEqual(ERROR)
   });
 
-  describe("multiple quizzes should have different id", () => {
+  test("multiple quizzes should have different id", () => {
     expect(adminQuizCreate(1, 'movie1', 'this is a movie')).
-    notToEqual(adminQuizCreate(1, 'movie2', 'this is a movie'));
+    not.toEqual(adminQuizCreate(1, 'movie2', 'this is a movie'));
   });
 });
