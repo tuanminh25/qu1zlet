089cea260dfa4068945d19e98386c581d9215d6a
Tracie Duong <z5420451@ad.unsw.edu.au>
2023-09-29 14:42:13 +1000

    adds a few more edge cases for adminquizcreate, such as invalid userid

--- src/quiz.test.js
+++ src/quiz.test.js
@@ -3,7 +3,32 @@ import {
   adminQuizDelete,
 } from './quiz.js';
 
+
 describe('adminQuizCreate', () => {
+  let data = {
+    users: [
+      {
+        userId: 1,
+        nameFirst: 'Hayden',
+        nameLast: 'Smith',
+        email: 'hayden.smith@unsw.edu.au',
+        password: 'hayden123',
+        numSuccessfulLogins: 3,
+        numFailedPasswordsSinceLastLogin: 1,
+      }
+    ],
+    quizzes: [
+      {
+        quizId: 1,
+        name: 'My Quiz',
+        timeCreated: 1683125870,
+        timeLastEdited: 1683125871,
+        description: 'This is my quiz',
+        ownedBy: 1,
+      }    
+    ]
+  }
+
   describe("test that the quiz is stored in data", () => {
     const uid = adminQuizCreate(1, 'Planet of the Apes', 'Vincent is an ape.');
     expect(data.quizzes).toContainObject({
@@ -12,15 +37,21 @@ describe('adminQuizCreate', () => {
       timeCreated: expect.any(Number),
       timeLastEdited: expect.any(Number),
       description: 'Vincent is an ape.',
-    }    )
+      ownedBy: 1,
+    });
+  });
+
+  describe("userid not contained in data", () => {
+    expect(adminQuizCreate(uid(), '21', 'whats 9 + 10')).
+    toStrictEqual({error: expect.any(String)})
   });
 
   describe("non-numerical input for id", () => {
-      expect(adminQuizCreate('hello', 'im', 'tracie')).
+      expect(adminQuizCreate(1, 'The Notebook', 'Cry')).
       toStrictEqual({error: expect.any(String)});
   });
 
-  const cases = [['', 'hello', 'hi'], ['one', '', 'two'], ['good', 'bye', '']];
+  const cases = [['', 'The Titanic', 'ship'], [1, '', 'two'], [1, 'Spiderman', '']];
   describe("blank inputs should create an error", () => {
     test.each(cases)((firstArg, secondArg, thirdArg) => {
       expect(adminQuizCreate(firstArg, secondArg, thirdArg)).
@@ -29,7 +60,7 @@ describe('adminQuizCreate', () => {
   });
 
   describe("multiple quizzes should have different id", () => {
-    expect(adminQuizCreate('tracie', 'movie1', 'this is a movie')).
-    notToEqual(adminQuizCreate('vincent', 'movie2', 'this is a movie'));
+    expect(adminQuizCreate(1, 'movie1', 'this is a movie')).
+    notToEqual(adminQuizCreate(1, 'movie2', 'this is a movie'));
   });
 });
