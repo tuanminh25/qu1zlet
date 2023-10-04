6c404e7fc6af32ca1aa6e21c672f98ef213a8592
Roger Duong <z5428838@ad.unsw.edu.au>
2023-10-04 14:59:37 +1100

    edit some white spaces and added JSDocs

--- src/helper.js
+++ src/helper.js
@@ -4,36 +4,29 @@ import { getData, setData } from "./dataStore.js";
 let store = getData();
 
 /**
-  * Given a an auth user id
+  * Given a quiz id
   *
-  * 
   * @param {string} quizId
   * @returns { user: 
-    *   {
-    *   quizId,
-    *   name,
-    *   timeCreated,
-    *   timeLastEdited,
-    *   description,
-    *   }
-    * }
-    * 
-    * 
-    * @returns {undefined} - quizId is not a valid quiz id 
-  */
+  *   {
+  *     quizId,
+  *     name,
+  *     timeCreated,
+  *     timeLastEdited,
+  *     description,
+  *   }
+  * }
+  * @returns {undefined} - quizId is not a valid quiz id 
+*/
 
-
-function checkquizId(quizId) {
+export function checkquizId(quizId) {
     const quiz = store.quizzes.find((quiz) => quiz.quizId === quizId);
     return quiz;
 }
 
-
-
 /**
   * Given a an auth user id
   *
-  * 
   * @param {string} authUserId
   * @returns { user: 
     *   {
@@ -46,13 +39,18 @@ function checkquizId(quizId) {
     * }
     * @returns {undefined} - AuthUserId is not a valid user
   */
-function checkauthUserId(authUserId) {
+export function checkauthUserId(authUserId) {
     const user = store.users.find((user) => user.userId === authUserId);
     return user;
 }
 
-
-function isPassword(password) {
+/**
+  * Checks if password is valid
+  * 
+  * @param {string} password
+  * @returns {boolean} 
+*/
+export function isPassword(password) {
     if (password.length < 8) {
       return false;
     } else if (/\d/.test(password) === false || /[a-zA-Z]/.test(password) === false) {
@@ -62,22 +60,21 @@ function isPassword(password) {
     }
   }
   
-  function isName(name) {
-    if (name.length < 2 || name.length > 20) {
-      return false;
-    } else if (/^[a-zA-Z\s'-]+$/.test(name) === false) {
-      return false;
-    } else {
-      return true;
-    }
+/**
+  * Checks if password is valid
+  * 
+  * @param {string} password
+  * @returns {boolean} 
+*/ 
+export function isUserName(name) {
+  if (name.length < 2 || name.length > 20) {
+    return false;
+  } else if (/^[a-zA-Z\s'-]+$/.test(name) === false) {
+    return false;
+  } else {
+    return true;
   }
-
-export {
-    checkauthUserId,
-    checkquizId,
-    isName,
-    isPassword,
-};
+}
 
 
 /**
