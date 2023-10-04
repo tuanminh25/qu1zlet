6802a248d44257810bdaa6bc043b2d2f4480654c
Vincent Le <z5425868@ad.unsw.edu.au>
2023-10-04 11:37:11 +1100

    documentation for adminQuizInfo

--- src/quiz.js
+++ src/quiz.js
@@ -1,3 +1,9 @@
+import { getData, setData } from "./dataStore.js";
+
+
+let store = getData();
+
+let quiz_id = 0;
 
 /**
     adminQuizDescriptionUpdate 
@@ -7,16 +13,77 @@
     Return object:
     { } empty object  
  * */
-
-
 function adminQuizDescriptionUpdate (authUserId, quizId, description) {
   return {
   }
 }
 
+/**
+  * Given basic details about a new quiz, create one for the logged in user. 
+  *  then returns a quizId.
+  * @param {number} authUserId
+  * @param {string} name 
+  * @param {string} description
+  * @returns {{ quizId: number }} 
+*/
 function adminQuizCreate(authUserId, name, description) {
+  if (store.quizzes.some((quiz) => quiz.name === name)) {
+    return {
+      error: 'Quiz name already exists'
+    };
+  } else if (!isQuizName(name)) {
+    return {
+      error: 'Invalid quiz name'
+    };
+  } else if (typeof(authUserId) !== "number") {
+    return {
+      error: 'User ID should be a number'
+    };
+  } else if (!isQuizDescription(description)) {
+    return {
+      error: 'Invalid quiz description'
+    };
+  }
+
+  const userExists = store.users.find((user) => user.userId === authUserId);
+  if (!userExists) {
+    return {
+      error: 'User not found'
+    };
+  }
+
+  const newQuiz = {
+    quizId: quiz_id++,
+    name: name,
+    timeCreated: Date.now() * 1000,
+    timeLastEdited: Date.now() * 1000,
+    description: description,
+    quizOwnedby: authUserId,
+  };
+
+  store.quizzes.push(newQuiz);
+  setData(store);
+
   return {
-      quizId: 2,
+      quizId: quiz_id,
+    }
+}
+
+function isQuizName(name) {
+  if (name.length < 3 || name.length > 30) {
+    return false;
+  } else if (/^[\w\s]+$/.test(name) === false) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function isQuizDescription(name) {
+  if (name.length > 100) {
+    return false;
+  } else {
+    return true;
   }
 }
 
@@ -51,7 +118,6 @@ function adminQuizList(authUserId) {
   }
 } - returns information if valid authUserId and quizId entered
 @returns {error: string} - invalid parameters entered
-
 **/
 function adminQuizInfo(authUserId, quizId) {
   return{
@@ -66,4 +132,10 @@ function adminQuizInfo(authUserId, quizId) {
 function adminQuizNameUpdate(authUserId, quizId, name){
   return{
   }
+}
+
+export {
+  adminQuizCreate,
+  adminQuizRemove,
+  adminQuizDescriptionUpdate
 }
