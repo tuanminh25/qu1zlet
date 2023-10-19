c6e18c0395cbca5029bf00e0b04218a09997c64c
Roger Duong <z5428838@ad.unsw.edu.au>
2023-10-19 20:25:36 +1100

    done clear implementation

---
+++ src/other.ts
@@ -0,0 +1,17 @@
+import {load, save} from './helper'
+
+/**
+  * Reset the state of the application back to the start.
+  * 
+  * @param {} - no parameter
+  * @returns {} - empty object
+*/
+export function clear() {
+  const data = load();
+  data.users.length = 0;
+  data.quizzes.length = 0;
+  data.sessions.length = 0;
+  save(data);
+
+  return {};
+}
