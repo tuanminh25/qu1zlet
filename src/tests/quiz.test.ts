eeba442a951b061f7369dd53171f3632f76d443a
Minh Truong <z5381092@ad.unsw.edu.au>
2023-10-27 04:17:58 +1100

    Update quiz.test.ts

--- src/tests/quiz.test.ts
+++ src/tests/quiz.test.ts
@@ -6,7 +6,7 @@ import {
   testQuizList,
   testCreateQuizQuestion,
   testQuizInfo,
-  testQuizDescriptionUpdate
+  testQuizNameUpdate
 } from './testHelper';
 
 const ERROR = { error: expect.any(String) };
@@ -45,11 +45,13 @@ describe('/v1/admin/quiz', () => {
   });
 
   test('error for duplicate names', () => {
-    testCreateQuiz(user.token, 'Dogs', 'I like cats');
-    const quiz = testCreateQuiz(user.token, 'Dogs', 'I like dogs');
-    expect(quiz.response).toStrictEqual(ERROR);
-    expect(quiz.status).toStrictEqual(400);
+    const quiz1 = testCreateQuiz(user.token, 'Dogs', 'I like cats');
+    expect(quiz1.status).toStrictEqual(200);
+    const quiz2 = testCreateQuiz(user.token, 'Dogs', 'I like dogs');
+    expect(quiz2.response).toStrictEqual(ERROR);
+    expect(quiz2.status).toStrictEqual(400);
   });
+
   test('Empty Quiz Name and Description', () => {
     const quiz = testCreateQuiz(user.token, '', '');
     expect(quiz.response).toStrictEqual(ERROR);
@@ -67,19 +69,12 @@ describe('/v1/admin/quiz', () => {
     expect(quiz2.status).toStrictEqual(400);
   });
 
-  test('Check 400 Error is Prioritized Over 401', () => {
+  test('Check 401 Error is Prioritized Over 400', () => {
     const invalidToken = user.token + 1;
     const emptyName = '';
     const quiz = testCreateQuiz(invalidToken, emptyName, 'A description of my quiz');
-
-    // Check first for 400 Error
     expect(quiz.response).toStrictEqual(ERROR);
-    expect(quiz.status).toStrictEqual(400);
-
-    // Then check for 401 Error with just the invalid token.
-    const quizWithInvalidToken = testCreateQuiz(invalidToken, 'My Quiz', 'A description of my quiz');
-    expect(quizWithInvalidToken.response).toStrictEqual(ERROR);
-    expect(quizWithInvalidToken.status).toStrictEqual(401);
+    expect(quiz.status).toStrictEqual(401);
   });
 });
 
@@ -480,6 +475,112 @@ describe('GET /v1/admin/quiz/:quizid', () => {
     const unauthorizedUser = testRegister('unauthorized@example.com', 'password123', 'Unauthorized', 'User').response;
     const quizinfo = testQuizInfo(unauthorizedUser.token, quiz.quizId);
     expect(quizinfo.status).toStrictEqual(403);
+  });
+});
+
+describe('/v1/admin/quiz/:quizid/name', () => {
+  let user: { token: string };
+  let quiz: { quizId: number };
+
+  beforeEach(() => {
+    testClear();
+    user = testRegister('testuser@example.com', 'Password123', 'Test', 'User').response;
+    quiz = testCreateQuiz(user.token, 'My Quiz Name', 'A description of my quiz').response;
+  });
+
+  test('Normal Name Change - Successful', () => {
+    const inforObjectOriginal = testQuizInfo(user.token, quiz.quizId).response;
+    const nameupdate = testQuizNameUpdate(user.token, quiz.quizId, 'New Quiz Name');
+    expect(nameupdate.response).toStrictEqual({});
+    expect(nameupdate.status).toStrictEqual(200);
+    const inforObjectNew = testQuizInfo(user.token, quiz.quizId).response;
+    expect(inforObjectNew.name).toStrictEqual('New Quiz Name');
+
+    expect(inforObjectOriginal.timeLastEdited !== inforObjectNew.timeLastEdited);
+  });
+
+  test.each([
+    { name: 'ABC' },
+    { name: 'A'.repeat(30) },
+  ])('Name Change with exactly $name characters - Successful', async ({ name }) => {
+    const infoObjectOriginal = testQuizInfo(user.token, quiz.quizId).response;
+    const nameUpdate = testQuizNameUpdate(user.token, quiz.quizId, name);
+
+    expect(nameUpdate.response).toStrictEqual({});
+    expect(nameUpdate.status).toStrictEqual(200);
+
+    const infoObjectNew = testQuizInfo(user.token, quiz.quizId).response;
+
+    expect(infoObjectNew.name).toStrictEqual(name);
+
+    expect(infoObjectNew.timeLastEdited !== infoObjectOriginal.timeLastEdited);
+  });
+
+  // Error Cases
+  describe('Name is not alphanumeric', () => {
+    test.each([
+      { name: 'Exclamation!' },
+      { name: 'Question?' },
+      { name: '@at' }
+    ])('Invalid Name: $name', async ({ name }) => {
+      const nameUpdate = testQuizNameUpdate(user.token, quiz.quizId, name);
+      const quizinfo = testQuizInfo(user.token, quiz.quizId).response;
+
+      expect(nameUpdate.response).toStrictEqual(ERROR);
+      expect(nameUpdate.status).toStrictEqual(400);
+
+      expect(quizinfo.name).toStrictEqual('My Quiz Name');
+    });
+
+    test('Name is less than 3 characters', () => {
+      const nameUpdate = testQuizNameUpdate(user.token, quiz.quizId, 'hi');
+      const quizinfo = testQuizInfo(user.token, quiz.quizId).response;
+
+      expect(nameUpdate.response).toStrictEqual(ERROR);
+      expect(nameUpdate.status).toStrictEqual(400);
+
+      expect(quizinfo.name).toStrictEqual('My Quiz Name');
+    });
+
+    test('Name is more than 30 characters', () => {
+      const nameUpdate = testQuizNameUpdate(user.token, quiz.quizId, 'A'.repeat(31));
+      const quizinfo = testQuizInfo(user.token, quiz.quizId).response;
+
+      expect(nameUpdate.response).toStrictEqual(ERROR);
+      expect(nameUpdate.status).toStrictEqual(400);
+
+      expect(quizinfo.name).toStrictEqual('My Quiz Name');
+    });
+
+    test('Quiz Name in use by same user', () => {
+      testCreateQuiz(user.token, 'NewQuiz', 'A description of my quiz');
+      const nameUpdate = testQuizNameUpdate(user.token, quiz.quizId, 'NewQuiz');
+      const quizinfo = testQuizInfo(user.token, quiz.quizId).response;
+
+      expect(nameUpdate.response).toStrictEqual(ERROR);
+      expect(nameUpdate.status).toStrictEqual(400);
+
+      expect(quizinfo.name).toStrictEqual('My Quiz Name');
+    });
+
+    test('Invalid Token', () => {
+      const nameUpdate = testQuizNameUpdate(user.token + 'abc', quiz.quizId, 'Quiz Name');
+      expect(nameUpdate.response).toStrictEqual(ERROR);
+      expect(nameUpdate.status).toStrictEqual(401);
+    });
+
+    test('Empty Token', () => {
+      const nameUpdate = testQuizNameUpdate('', quiz.quizId, 'Quiz Name');
+      expect(nameUpdate.response).toStrictEqual(ERROR);
+      expect(nameUpdate.status).toStrictEqual(401);
+    });
+
+    test('Unauthorized', () => {
+      const unauthorizedUser = testRegister('unauthorized@example.com', 'password123', 'Unauthorized', 'User').response;
+      const nameUpdate = testQuizNameUpdate(unauthorizedUser.token, quiz.quizId, 'Quiz Name');
+      expect(nameUpdate.response).toStrictEqual(ERROR);
+      expect(nameUpdate.status).toStrictEqual(403);
+    });
   });
 });
 
