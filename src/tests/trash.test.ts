0bad3ec2c1454ae2e7ab193a885d16828ba82f6f
Minh Truong <z5381092@ad.unsw.edu.au>
2023-11-11 03:08:21 +1100

    adding tests to emptyTrash v1

--- src/tests/trash.test.ts
+++ src/tests/trash.test.ts
@@ -7,7 +7,8 @@ import {
   testRestoreTrash,
   testQuizList,
   testCreateQuizQuestion,
-  testQuizInfo
+  testQuizInfo,
+  emptyTheTrash
 } from './testHelper';
 
 const ERROR = { error: expect.any(String) };
@@ -346,5 +347,109 @@ describe('Restore Trash', () => {
     );
   });
 });
+
+describe.only('Empty the trash', () => {
+  let user: { token: string };
+  let quiz: { quizId: number };
+  beforeEach(() => {
+    testClear();
+    user = testRegister('testuser@example.com', 'Password123', 'Test', 'User').response;
+    quiz = testCreateQuiz(user.token, 'My Quiz Name', 'A description of my quiz').response;
+    testQuizToTrash(user.token, quiz.quizId).response;
+
+
+  });
+
+
+  // Working cases
+  test.only('Many quizies in the trash', () => {
+    // Create many quizies
+    const quiz2 = testCreateQuiz(user.token, 'Another quiz', 'Yes sir').response;
+    const quiz3 = testCreateQuiz(user.token, 'Yes moree quiz', 'Yahooo').response;
+    const quiz4 = testCreateQuiz(user.token, 'Even bigger', 'Yellow').response;
+    const quiz5 = testCreateQuiz(user.token, 'Can u imagine it', 'No').response;
+    const quiz6 = testCreateQuiz(user.token, 'I need to know now', 'Can u love me again?').response;
+
+
+    const sendToTrash4 = testQuizToTrash(user.token, quiz4.quizId);
+    const sendToTrash6 = testQuizToTrash(user.token, quiz6.quizId);
+
+
+    let inTrash = testViewTrash(user.token).response.quizzes;
+    expect(inTrash).toStrictEqual([
+      {
+        quizId: quiz.quizId,
+        name: 'My Quiz Name'
+      },
+      {
+        quizId: quiz4.quizId,
+        name: 'Even bigger'
+      },
+      {
+        quizId: quiz6.quizId,
+        name: 'I need to know now'
+      },
+    ])
+
+
+    const removeIds = [];
+    removeIds.push(quiz4.quizId);
+    removeIds.push(quiz.quizId);
+    removeIds.push(quiz6.quizId);
+
+
+    const updateResponse = emptyTheTrash(user.token, removeIds);
+
+
+    expect(inTrash).toStrictEqual({})
+    expect(updateResponse.response).toStrictEqual({});
+    expect(updateResponse.status).toStrictEqual(200);
+  });
+
+
+
+
+
+
+  // Error cases:
+  test('QuizId is not in the trash', () => {
+    const quiz2 = testCreateQuiz(user.token, 'Another quiz', 'Yes sir').response;
+    const remove = [quiz2.quizId]
+    const updateResponse = emptyTheTrash(user.token, remove);
+    expect(updateResponse.response).toStrictEqual(ERROR);
+    expect(updateResponse.status).toStrictEqual(400);
+  });
+
+
+
+
+  // Token is empty or invalid (does not refer to valid logged in user session)
+  test('Token is empty or invalid', () => {
+    const remove = [quiz.quizId]
+    const updateResponse = emptyTheTrash(user.token + 1, remove);
+    expect(updateResponse.response).toStrictEqual(ERROR);
+    expect(updateResponse.status).toStrictEqual(401);
+  });
+
+
+
+
+  // Valid token is provided, but one or more of the Quiz IDs refers to a quiz that this current user does not own
+  test('Quiz ID does not refer to a quiz that this user owns, belongs to somebody else', () => {
+    const user2 = testRegister('somebody123@unsw.edu.au', 'password2', 'nameFirs', 'nameLas').response;
+    const quiz2 = testCreateQuiz(user2.token, 'Quiz by user 2', 'User 2 quiz').response;
+    expect(testQuizToTrash(user2.token, quiz2.quizId).status).toStrictEqual(200);
+   
+    const remove = [quiz2.quizId]
+    const updateResponse = emptyTheTrash(user.token, remove);
+    expect(updateResponse.response).toStrictEqual(ERROR);
+    expect(updateResponse.status).toStrictEqual(403);
+  });
+
+
+
+
+})
+
 
 testClear();
