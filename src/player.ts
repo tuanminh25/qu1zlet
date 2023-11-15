e8bbd4aebdcc553053ae8b6e65f22f5d0ab0ea01
Roger Duong <z5428838@ad.unsw.edu.au>
2023-11-15 23:58:57 +1100

    done implementation

--- src/player.ts
+++ src/player.ts
@@ -1,14 +1,39 @@
 import {
   load,
-  isPLayer,
+  findPlayerFromId,
+  generateTime,
+  save
 } from './helper';
-
-export function getChatMessages(playerId: number) {
+import { ChatMessage } from './interface';
+import HttpError from 'http-errors';
+export function getChatMessages(playerId: number): {messages: ChatMessage[]}  {
   const data = load();
-  const sessionId = isPLayer(playerId);
-  const gameSession = data.gameSessions.find((g) => g.gameSessionId === sessionId);
+  const player = findPlayerFromId(playerId);
+  const gameSession = data.gameSessions.find((g) => g.gameSessionId === player.sessionId);
 
   return {
     messages: gameSession.messages
   };
+}
+
+export function sendChatMessages(playerId: number, message: string): Record<string, never>  {
+  const data = load();
+  const player = findPlayerFromId(playerId);
+  const gameSession = data.gameSessions.find((q) => q.gameSessionId === player.sessionId);
+
+  if (message.length < 1 || message.length > 100) {
+    throw HttpError(400, 'Invalid message length');
+  }
+
+  const newChatMessage: ChatMessage = {
+    playerId: playerId,
+    messageBody: message,
+    timeSent: generateTime(),
+    playerName: player.name
+  };
+
+  gameSession.messages.push(newChatMessage);
+  save(data);
+
+  return {};
 }
