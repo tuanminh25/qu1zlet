761f46dd9bdd1f0043bc2ea2718364abf73fa7a6
Jeffrey <z5419375@ad.unsw.edu.au>
2023-11-17 01:00:05 +1100

    finished implementing the playerQuestionResult function

---
+++ src/result.ts
@@ -0,0 +1,37 @@
+import {
+    load,
+    findPlayerFromId,
+    checkQuesPosition
+  } from './helper';
+import { GameState, ReturnQuestResult } from './interface';
+import HttpError from 'http-errors';
+
+export function playerQuestionResult(playerId: number, questionPosition: number): ReturnQuestResult {
+  const data = load();
+  const player = findPlayerFromId(playerId);
+  // findPlayerFromId() takes care of invalid player ID
+
+  const gameSession = data.gameSessions.find((g) => g.gameSessionId === player.sessionId);
+
+  if (!checkQuesPosition(gameSession, questionPosition)) {
+    throw HttpError(400, 'question position is not valid for the session this player is in');
+  }
+
+  if (gameSession.state !== GameState.ANSWER_SHOW) {
+    throw HttpError(400, 'session is not in ANSWER_SHOW state');
+  }
+
+  if (gameSession.atQuestion !== questionPosition) {
+    throw HttpError(400, 'session is not yet up to this question');
+  }
+
+  const questionData = gameSession.questionDatas[questionPosition];
+  const playerList = questionData.playersCorrectList.map(player => player.name);
+
+  return {
+    questionId: questionData.questionId,
+    playersCorrectList: playerList,
+    averageAnswerTime: questionData.averageAnswerTime,
+    percentCorrect: questionData.percentCorrect
+  };
+}
